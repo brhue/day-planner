b61242c70b27c85348b9a096513ec39425f29056
Bradley Donahue <donahue.bradley@gmail.com>
2020-12-17 09:46:08 -0500

    Style rows based on time

--- script.js
+++ script.js
@@ -4,20 +4,39 @@ let containerEl = $('.container');
 
 // VARIABLES
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let now = luxon.DateTime.local();
 
-currentDayEl.text(luxon.DateTime.local().toLocaleString(luxon.DateTime.DATE_HUGE));
+currentDayEl.text(now.toLocaleString(luxon.DateTime.DATE_HUGE));
 
 hours.forEach(hour => {
+  let currentHour = now.hour;
+  let whenInTime = '';
+  if (hour < currentHour) {
+    whenInTime = 'past';
+  } else if (hour == currentHour) {
+    whenInTime = 'present';
+  } else {
+    whenInTime = 'future';
+  }
+  createRow(hour, whenInTime);
+});
+
+function createRow(hour, when) {
   let row = $('<div class="row">');
   let timeCol = $('<div class="col">');
   let inputCol = $('<div class="col">');
   let saveCol = $('<div class="col">');
 
   let hourEl = $('<p class="hour">');
-  hourEl.text(hour);
+  if (hour > 12) {
+    hour -= 12;
+    hourEl.text(hour + 'PM');
+  } else {
+    hourEl.text(hour + 'AM');
+  }
   timeCol.append(hourEl);
 
-  let textArea = $('<textarea>');
+  let textArea = $('<textarea class="' + when + '">');
   inputCol.append(textArea);
 
   let saveBtn = $('<button class="saveBtn">');
@@ -28,4 +47,4 @@ hours.forEach(hour => {
 
   row.append(timeCol, inputCol, saveCol);
   containerEl.append(row);
-});
+}
